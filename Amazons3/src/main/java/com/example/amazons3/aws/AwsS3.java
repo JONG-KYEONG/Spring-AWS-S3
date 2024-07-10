341d6b4dd01d0cc1b5bfc6b74d30b095a6dc1bdc
김종경 <111286262+JONG-KYEONG@users.noreply.github.com>
2024-07-10 14:23:11 +0900

    Update AwsS3.java

--- Amazons3/src/main/java/com/example/amazons3/aws/AwsS3.java
+++ Amazons3/src/main/java/com/example/amazons3/aws/AwsS3.java
@@ -1,14 +1,11 @@
 package com.example.amazons3.aws;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class AwsS3 {
     private String key;  // 파일명
     private String path;  // url
-
-    public AwsS3(String key, String path) {
-        this.key = key;
-        this.path = path;
-    }
 }
